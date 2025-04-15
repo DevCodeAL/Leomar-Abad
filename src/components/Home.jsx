0d88d77c9402b7608cf7a6b24d08eb937abd568f
DevCodeAl <abadleomar875@gmail.com>
2025-04-15 15:18:48 +0800

    commit all updates in my portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,33 @@
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
 export default function Home(){
     return(
         <>
-           <section className="flex justify-center w-full border-2 border-red-600 h-screen" id="home">
-                 <div>
-                      <h1 className="text-3xl mt-20">Home</h1>
-                 </div>
+           <section className="flex justify-center items-center w-full bg-[#121212] z-10 h-screen" id="home">
+                    <div className="flex flex-row gap-60">
+                        <div className="basis-2/3 w-96 h-96 p-6 pt-12 text-white">
+                        <h1 className="text-4xl font-bold leading-10">I'm, Leomar Abad, a</h1>
+                        <h1 className="text-5xl font-bold text-[#1ed760] text-nowrap">Fullstack Web Developer</h1>
+                        <p className="pt-5 leading-6">I’m a Fullstack Web Developer specializing in building modern, responsive web applications using the MERN stack. I turn ideas into fast, scalable, and user-friendly digital experiences.</p>
+                        <div className="flex justify-start gap-4 pt-3">
+                            <div className="text-2xl"><FaFacebook/></div>
+                            <div className="text-2xl"><FaInstagramSquare/></div>
+                            <div className="text-2xl"><FaLinkedin/></div>
+                            <div className="text-2xl"><FaGithub/></div>
+                        </div>
+                        </div>
+
+                        <div className="relative basis-2/3 w-96 h-96 rounded-full overflow-hidden">
+                        {/* Light effect behind image */}
+                        <div className="absolute inset-0 bg-white opacity-5 blur-3xl z-0"></div>
+                        {/* Image on top */}
+                        <img src="/image/Leomar-Abad.png" className="relative w-full h-full object-cover z-10 rounded-full" alt="Picture" />
+                        </div>
+
+                    </div>
            </section>
         </>
     );
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,28 +2,32 @@
 export default function NavigationBar(){
     return(
         <>
-            <div className="flex justify-end items-center fixed w-full bg-blue-950 text-white p-3 gap-6">
-               <div>
+            <div className="flex justify-end items-center fixed z-20 w-full bg-[#121212] font-bold p-3 gap-10 pr-10 border-b-2 border-[#1ed760]">
+                <div className="absolute left-6">
+                    <img src="/image/logo.png" className="w-16 h-auto" alt="Logo" />
+                </div>
+
+               <div className="text-[#FFFFFF] hover:text-[#1ed760]">
                     <a href="#home">Home</a>
                </div>
 
-               <div>
+               <div className="text-[#FFFFFF] hover:text-[#1ed760]">
                     <a href="#about">About Me</a>
                </div>
 
-               <div>
+               <div className="text-[#FFFFFF] hover:text-[#1ed760]">
                     <a href="#services">Services</a>
                </div>
 
-                <div>
+                <div className="text-[#FFFFFF] hover:text-[#1ed760]">
                   <a href="#skills">Skills</a>
                 </div>
 
-                <div>
+                <div className="text-[#FFFFFF] hover:text-[#1ed760]">
                     <a href="#projects">Projects</a>
                 </div>
                    
-                   <div>
+                   <div className="text-[#FFFFFF] hover:text-[#1ed760]">
                       <a href="#contact">Contact</a>
                    </div>
                </div>
