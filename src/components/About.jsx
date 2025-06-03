10b39b5c618d9b8b66615e39c68e9535631fb673
DevCodeAl <abadleomar875@gmail.com>
2025-06-03 21:19:33 +0800

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,8 +14,8 @@ export default function About(){
                     </div>
                 </div>
 
-                <div className="w-[400px] sm:w-[480px] md:w-[640px] lg:w-[800px] px-4">
-                <p className="text-white text-center text-[14px] sm:text-sm md:text-sm lg:text-base  md:whitespace-normal mx-3">
+                <div className="w-[320px] sm:w-[480px] md:w-[640px] lg:w-[800px] px-4">
+                <p className="text-white text-center text-[14px] sm:text-sm md:text-sm lg:text-base  md:whitespace-normal px-2">
                     I’m a passionate Fullstack Web Developer with solid experience in building responsive, scalable web applications using the MERN stack (MongoDB, Express, React, Node.js and Tailwind CSS). I specialize in crafting clean, user-friendly solutions that deliver real value to users and businesses alike.
 
                     My professional goal is to design and develop digital products that solve real-world problems and address complex business logic. I’m driven by continuous learning, and I strive to create high-performance applications that are both functional and visually engaging through strong UI/UX practices.
